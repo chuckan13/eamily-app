3fea2ea67f06b843d3505f8c011a27212137f13e
PlatGod <an.charlie@gmail.com>
2019-07-12 15:35:36 +0300

    sdf

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const keys = require("./config/keys");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 
@@ -8,6 +10,14 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
