f98236f455797df190858f9fc50a5fd9ebd41c42
PlatGod <an.charlie@gmail.com>
2019-07-12 16:21:34 +0300

    sdf

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKE_KEY
+};
+
+// 305521652917-8vpqne86382oiohumsc6fcbgein863gh.apps.googleusercontent.com
+
+// jgho-r5kLqG7CXGNka9oDCoV
