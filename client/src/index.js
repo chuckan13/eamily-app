00b445db54edb0b16d4377e9e991d0bac652f796
PlatGod <an.charlie@gmail.com>
2019-08-21 10:19:03 -0400

    sdf

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+// axios dev only, delete for prod
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
